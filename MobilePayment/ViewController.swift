dd84dd436fce9a2eb59cb2d7a538aebeb143a35b
Leo Trieu <trieuquanganh@gmail.com>
2016-08-04 21:07:37 +1000

    Initial Commit

---
+++ MobilePayment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MobilePayment
+//
+//  Created by Leo Trieu on 4/08/2016.
+//  Copyright © 2016 Leo Trieu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
